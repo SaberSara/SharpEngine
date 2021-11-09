52b4429c17f136a886b1212558d61cc590d0235b
Sara SABER <sarasaberdev@gmail.com>
2021-11-09 13:36:28 +0300

    Add Triangle Verticies

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using GLFW;
+using static OpenGL.Gl;
 
 namespace SharpEngine
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Glfw.Init();
+
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
+            Import(Glfw.GetProcAddress);
 
-            while (!Glfw.WindowShouldClose(window))
-            {
-                Glfw.PollEvents(); //react to window changes (positions, etc.)
-                //do nothing
+            float[] vertices = new float[] {
+                -.5f, -.5f, 0f,
+                .5f, -.5f, 0f,
+                0f, .5f, 0f
+            };
+
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            unsafe {
+                fixed (float* vertex = &vertices[0]) {
+                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+                }
+                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+            }
+            glEnableVertexAttribArray(0);
+
+            while (!Glfw.WindowShouldClose(window)) {
+                Glfw.PollEvents(); // react to window changes (position etc.)
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+                Glfw.SwapBuffers(window);
             }
         }
     }
