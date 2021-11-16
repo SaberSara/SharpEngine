d381aa5d8d1f9bb46683f78c356dd999ebfeecd2
Sara SABER <sarasaberdev@gmail.com>
2021-11-16 18:07:56 +0300

    Add inputs and forward direction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using GLFW;
 
 namespace SharpEngine
@@ -19,7 +20,8 @@ namespace SharpEngine
             var scene = new Scene();
             window.Load(scene);
 
-            var shape = new Circle(material);
+            var shape = new Triangle(material);
+            shape.Transform.CurrentScale = new Vector(0.5f, 1f, 1f);
             scene.Add(shape);
 
             var ground = new Rectangle(material);
@@ -29,12 +31,38 @@ namespace SharpEngine
 
             // engine rendering loop
             const int fixedStepNumberPerSecond = 30;
-            const double fixedStepDuration = 1.0 / fixedStepNumberPerSecond;
+            const float fixedDeltaTime = 1.0f / fixedStepNumberPerSecond;
+            const float movementSpeed = 0.5f;
             double previousFixedStep = 0.0;
             while (window.IsOpen()) {
-                while (Glfw.Time > previousFixedStep + fixedStepDuration) {
-                    previousFixedStep += fixedStepDuration;
-                    
+                while (Glfw.Time > previousFixedStep + fixedDeltaTime) {
+                    previousFixedStep += fixedDeltaTime;
+                    var walkDirection = new Vector();
+                    if (window.GetKey(Keys.W)) {
+                        walkDirection += shape.Transform.Forward;
+                    }
+                    if (window.GetKey(Keys.S)) {
+                        walkDirection += Vector.Backward;
+                    }
+                    if (window.GetKey(Keys.A)) {
+                        walkDirection += Vector.Left;
+                    }
+                    if (window.GetKey(Keys.D)) {
+                        walkDirection += Vector.Right;
+                    }
+                    if (window.GetKey(Keys.Q)) {
+                        var rotation = shape.Transform.Rotation;
+                        rotation.z += MathF.PI * fixedDeltaTime;
+                        shape.Transform.Rotation = rotation;
+                    }
+                    if (window.GetKey(Keys.E)) {
+                        var rotation = shape.Transform.Rotation;
+                        rotation.z -= MathF.PI * fixedDeltaTime;
+                        shape.Transform.Rotation = rotation;
+                    }
+
+                    walkDirection = walkDirection.Normalize();
+                    shape.Transform.Position += walkDirection * movementSpeed * fixedDeltaTime;
                 }
                 window.Render();
             }
