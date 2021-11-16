b868647a4a0b3497c96c09e6d4fc72e59f484817
Sara SABER <sarasaberdev@gmail.com>
2021-11-16 15:46:37 +0300

    Small changes createCircle and createRectangle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using GLFW;
 
 namespace SharpEngine
 {
@@ -12,20 +12,6 @@ namespace SharpEngine
             return Lerp(min, max, (float)random.Next() / int.MaxValue);
         }
         
-        static void FillSceneWithTriangles(Scene scene, Material material) {
-            var random = new Random();
-            for (var i = 0; i < 10; i++) {
-                var triangle = new Triangle(new Vertex[] {
-                    new Vertex(new Vector(-.1f, 0f), Color.Red),
-                    new Vertex(new Vector(.1f, 0f), Color.Green),
-                    new Vertex(new Vector(0f, .133f), Color.Blue)
-                }, material);
-                triangle.Transform.Rotate(GetRandomFloat(random));
-                triangle.Transform.Move(new Vector(GetRandomFloat(random, -1, 1), GetRandomFloat(random, -1, 1)));
-                scene.Add(triangle);
-            }
-        }
-        
         static void Main(string[] args) {
             
             var window = new Window();
@@ -33,49 +19,23 @@ namespace SharpEngine
             var scene = new Scene();
             window.Load(scene);
 
-            //FillSceneWithTriangles(scene, material);
-            var newTriangle = new Triangle(new Vertex[] {
-                new Vertex(new Vector(-.1f, 0f), Color.Red),
-                new Vertex(new Vector(.1f, 0f), Color.Green),
-                new Vertex(new Vector(0f, .133f), Color.Blue)
-            }, material);
-            scene.Add(newTriangle);
-            
-            // engine rendering loop
-            var direction = new Vector(0.0003f, 0.0003f);
-            var multiplier = 0.999f;
-            var rotation = 0.0005f;
-            while (window.IsOpen()) {
+            var shape = new Circle(material);
+            scene.Add(shape);
 
-                // Update Triangles
-                for (var i = 0; i < scene.triangles.Count; i++) {
-                    var triangle = scene.triangles[i];
-                
-                    // 2. Keep track of the Scale, so we can reverse it
-                    if (triangle.Transform.CurrentScale.GetMagnitude() <= 0.5f) {
-                        multiplier = 1.001f;
-                    }
-                    if (triangle.Transform.CurrentScale.GetMagnitude() >= 2f) {
-                        multiplier = 0.999f;
-                    }
+            var ground = new Rectangle(material);
+            ground.Transform.CurrentScale = new Vector(10f, 1f, 1f);
+            ground.Transform.Position = new Vector(0f, -1f);
+            scene.Add(ground);
+
+            // engine rendering loop
+            const int fixedStepNumberPerSecond = 30;
+            const double fixedStepDuration = 1.0 / fixedStepNumberPerSecond;
+            double previousFixedStep = 0.0;
+            while (window.IsOpen()) {
+                while (Glfw.Time > previousFixedStep + fixedStepDuration) {
+                    previousFixedStep += fixedStepDuration;
                     
-                    triangle.Transform.Scale(multiplier);
-                    triangle.Transform.Rotate(rotation);
-                
-                    // 4. Check the X-Bounds of the Screen
-                    if (triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0) {
-                        direction.x *= -1;
-                    }
-                
-                    // 5. Check the Y-Bounds of the Screen
-                    if (triangle.GetMaxBounds().y >= 1 && direction.y > 0 || triangle.GetMinBounds().y <= -1 && direction.y < 0) {
-                        direction.y *= -1;
-                    }
-                    
-                    
-                    triangle.Transform.Move(direction);
                 }
-                
                 window.Render();
             }
         }
